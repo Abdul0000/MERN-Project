05c77d596cc737092592d78ef372d0873efc490e
Abdul0000 <qudddos@gmail.com>
2024-12-10 23:06:49 +0500

    t

--- GptClone/client/src/components/Chats.jsx
+++ GptClone/client/src/components/Chats.jsx
@@ -10,76 +10,86 @@ import { store } from "../ReduxStore/store.js";
 import { setResponses } from "../ReduxStore/Features/historySlice.js";
 
 const Chats = () => {
-    const isLogin = useSelector(state => state.auth.user)
-    const responses = useSelector(state => state.history.responses)
-    const dbHistory = useSelector(state => state.history.fullHistroy)
-    const check = useSelector(state => state.history.check)
-    // console.log(responses)
-    const email = isLogin? isLogin.email:''
-    const userInputRef = useRef();
-    const chatEndRef = useRef(null);
-    const [popup,setPopup] = useState(false)
-    const [signupPopup,setSignupPopup] = useState(false)
-    const [forgetPopup,setForgetPopup] = useState(false)
-    
-    // const [responses, setResponses] = useState([]);
-    const genAI = new GoogleGenerativeAI("AIzaSyAoJRj0bzp6AoVHS7ppz7IB_VW2aq7CtpY");
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const isLogin = useSelector((state) => state.auth.user);
+  const responses = useSelector((state) => state.history.responses);
+  const dbHistory = useSelector((state) => state.history.fullHistroy);
+  const check = useSelector((state) => state.history.check);
+  // console.log(responses)
+  const email = isLogin ? isLogin.email : "";
+  const userInputRef = useRef();
+  const chatEndRef = useRef(null);
+  const [popup, setPopup] = useState(false);
+  const [signupPopup, setSignupPopup] = useState(false);
+  const [forgetPopup, setForgetPopup] = useState(false);
 
-    const getResponseFromAI = async () => {
+  // const [responses, setResponses] = useState([]);
+  const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const getResponseFromAI = async () => {
     const prompt = userInputRef.current.value.trim();
     if (prompt) {
-        const result = await model.generateContent(`${prompt} \`\`\`Correct and relevant to prompt not too long\`\`\`context`);
-        if (result) {
-            // store.dispatch(setCheck())
-            store.dispatch(setResponses({prompt,result:result.response.text()}));
-        }
+      const result = await model.generateContent(
+        `${prompt} \`\`\`Correct and relevant to prompt not too long\`\`\`context`
+      );
+      if (result) {
+        // store.dispatch(setCheck())
+        store.dispatch(
+          setResponses({ prompt, result: result.response.text() })
+        );
+      }
     }
-    };
+  };
 
+  let finalData = responses;
+  console.log(check);
+  if (dbHistory && check) {
+    finalData = dbHistory;
+  }
 
-    let finalData = responses
-    console.log(check)
-    if(dbHistory && check){
-        finalData = dbHistory
-    }
-    
-    const handleOnClick = () => {
+  const handleOnClick = () => {
     getResponseFromAI();
-    userInputRef.current.value = '';
-    };
+    userInputRef.current.value = "";
+  };
 
-    useEffect(() => {
+  useEffect(() => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [responses]);
-    
-    const handOnLogin = ()=>{
-    setPopup(true)
-    }
-    const handOnLogout = ()=>{
-    localStorage.clear()
-    store.dispatch(getLogout())
-    }
+  }, [responses]);
+
+  const handOnLogin = () => {
+    setPopup(true);
+  };
+  const handOnLogout = () => {
+    localStorage.clear();
+    store.dispatch(getLogout());
+  };
 
   return (
     <div className="bg-slate-600 flex flex-col items-center w-full h-screen p-2 pb-6">
-    
       <header className="m-6 flex items-baseline justify-between w-11/12">
         <h1 className="text-4xl font-semibold text-gray-100">ChatGPT</h1>
-        
-        {
-        isLogin ?<div className="flex items-center justify-center gap-4 -pt-10">{email&&<p className="text-gray-100">{email}</p>}
-        <button onClick={handOnLogout} className="text-xl bg-white hover:bg-gray-200 text-gray-500 font-semibold w-24 h-11 rounded">
-          Log Out
-        </button></div>:
-        <button onClick={handOnLogin} className="text-xl bg-white hover:bg-gray-200 text-gray-500 font-semibold w-24 h-11 rounded">
-          Login
-        </button>
-        }
-       
+
+        {isLogin ? (
+          <div className="flex items-center justify-center gap-4 -pt-10">
+            {email && <p className="text-gray-100">{email}</p>}
+            <button
+              onClick={handOnLogout}
+              className="text-xl bg-white hover:bg-gray-200 text-gray-500 font-semibold w-24 h-11 rounded"
+            >
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={handOnLogin}
+            className="text-xl bg-white hover:bg-gray-200 text-gray-500 font-semibold w-24 h-11 rounded"
+          >
+            Login
+          </button>
+        )}
       </header>
       {/* <div>{email&&<p className="text-xl text-gray-100 -mt-[100px] ml-[1250px] ">{email}</p>}</div>  */}
-      
+
       <main className="flex flex-col flex-grow w-full max-w-6xl bg-slate-700 rounded-lg shadow-md overflow-hidden">
         <div className="flex flex-col flex-grow overflow-y-auto p-4 space-y-6">
           {finalData.length > 0 ? (
@@ -105,7 +115,8 @@ const Chats = () => {
             ))
           ) : (
             <div className="flex items-center justify-center text-4xl font-semibold text-gray-100 h-full">
-              <span className="text-red-300">How</span> <p className="ml-2">can I help you !!!</p>
+              <span className="text-red-300">How</span>{" "}
+              <p className="ml-2">can I help you !!!</p>
             </div>
           )}
           <div ref={chatEndRef}></div>
@@ -125,9 +136,29 @@ const Chats = () => {
           />
         </div>
       </footer>
-      {popup ? <LogIn setPopup={setPopup} setSignupPopup={setSignupPopup} setForgetPopup={setForgetPopup}/>:''}
-      {forgetPopup ? <ForgetPassword setPopup={setPopup} setSignupPopup={setSignupPopup} setForgetPopup={setForgetPopup}/>:''}
-      {signupPopup ? <Signup setSignupPopup={setSignupPopup} setPopup={setPopup}/>:''}
+      {popup ? (
+        <LogIn
+          setPopup={setPopup}
+          setSignupPopup={setSignupPopup}
+          setForgetPopup={setForgetPopup}
+        />
+      ) : (
+        ""
+      )}
+      {forgetPopup ? (
+        <ForgetPassword
+          setPopup={setPopup}
+          setSignupPopup={setSignupPopup}
+          setForgetPopup={setForgetPopup}
+        />
+      ) : (
+        ""
+      )}
+      {signupPopup ? (
+        <Signup setSignupPopup={setSignupPopup} setPopup={setPopup} />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
